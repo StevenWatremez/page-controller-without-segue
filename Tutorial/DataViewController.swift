a97a11959ee1c88b29f9438483d553fa3da7c715
Steven Watremez <steven.watremez@niji.fr>
2017-06-14 11:46:12 +0200

    delete unused code

--- Tutorial/DataViewController.swift
+++ Tutorial/DataViewController.swift
@@ -28,11 +28,6 @@ class DataViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     self.dataLabel.text = dataObject?.title
